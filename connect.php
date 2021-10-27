f51546aa86b1d41e3f6927eb66ce03c4a60c6196
Artur Malyshev <42480216+ArturMalyshev@users.noreply.github.com>
2021-10-27 15:08:27 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,15 +1,14 @@
 <?php
     $host='localhost';
-    $user='z202';
-    $pass='7!2fV:uJD8tk!VY';
-    $database='z202';
+    $user='insert login';
+    $pass='inser password';
+    $database='insert database name';
 
     $link=new mysqli($host,$user,$pass,$database);
     if($link -> connect_erno){
-        echo'проблема с БД';
+        echo'errors in database';
     }
     else{
-        //echo'норм бд ! ';
         $link -> set_charset("utf8");
     }
 ?>
